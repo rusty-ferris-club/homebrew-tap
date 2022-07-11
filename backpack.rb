86e0ae90cbb7de33452de07e7aa2277e5a64c026
Dotan J. Nahum <jondotan@gmail.com>
2022-07-11 10:10:53 +0300

    updating formula for backpack

--- backpack.rb
+++ backpack.rb
@@ -1,12 +1,12 @@
 class Backpack < Formula
     desc "Create projects from existing repos"
     homepage "https://github.com/rusty-ferris-club/backpack"
-    version "-----"
+    version "1.0.5"
 
     on_macos do
         if Hardware::CPU.arm?
-            url "-----"
-            sha256 "-----"
+            url "https://github.com/rusty-ferris-club/backpack/releases/download/v1.0.5/backpack-v1.0.5-x86_64-macos.tar.xz"
+            sha256 "344e2fe0632b1eec550f368f50de0e80eb9bd896e8104dff9b93221c6be64208"
         end
         if Hardware::CPU.intel?
             url "-----"
