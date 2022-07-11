187266af9a16d35cf9dcec80f94357a10c001e25
Dotan J. Nahum <jondotan@gmail.com>
2022-07-11 13:32:57 +0300

    Update backpack.rb

--- backpack.rb
+++ backpack.rb
@@ -1,9 +1,9 @@
 class Backpack < Formula
     desc "Create projects from existing repos"
     homepage "https://github.com/rusty-ferris-club/backpack"
-    version "1.0.7"
-    url "https://github.com/rusty-ferris-club/backpack/releases/download/v1.0.7/backpack-v1.0.7-x86_64-macos.tar.xz"
-    sha256 "81cba1d804252b7a89370925ed98affc4b27ceb8549f4f92d251610f9d7d02ad"
+    version "-----"
+    url "-----"
+    sha256 "-----"
 
     def install
       bin.install "bp"
