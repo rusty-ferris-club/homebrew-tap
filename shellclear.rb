6ec2ec3b67c12c92a5e4b32d4538f7cbfb820e8a
Elad Kaplan <kaplan.elad@gmail.com>
2022-07-12 21:38:10 +0300

    Update shellclear.rb

--- shellclear.rb
+++ shellclear.rb
@@ -1,9 +1,9 @@
 class Shellclear < Formula
     desc "Secure shell history commands"
     homepage "https://github.com/rusty-ferris-club/shellclear"
-    version "-----"    
-    url "-----"
-    sha256 "-----"
+    version "0.2.0"    
+    url "https://github.com/rusty-ferris-club/shellclear/releases/download/v0.2.0/shellclear-v0.2.0-x86_64-macos.tar.xz"
+    sha256 "289d47874aca22cd295635cfcbe27eb7c373b3ad035e61be556e3ed9768d4a4e"
     
     def install
       bin.install "shellclear"
