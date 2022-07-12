11559e5514691bcdbe5cfa901b54cf7d6193d671
Dotan J. Nahum <jondotan@gmail.com>
2022-07-12 18:52:29 +0300

    Update backpack.rb

--- backpack.rb
+++ backpack.rb
@@ -6,7 +6,7 @@ class Backpack < Formula
     sha256 "a7ea64a2a2e44b1361f598018d78625d6145fbf28b5d0fcc029aa5a793c10ec5"
 
     def install
-      ln_s libexec/"bp", libexec/"backpack"
+      ln_s bin/"bp", bin/"backpack"
       bin.install "bp"
       bin.install_symlink "backpack"
     end
