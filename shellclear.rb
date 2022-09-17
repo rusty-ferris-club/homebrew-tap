a871c12b087285ddb2afab7c9130ac23fa5b013e
Elad Kaplan <kaplan.elad@gmail.com>
2022-09-17 20:37:29 +0300

    updating formula for shellclear

--- shellclear.rb
+++ shellclear.rb
@@ -1,9 +1,9 @@
 class Shellclear < Formula
     desc "Secure shell history commands"
     homepage "https://github.com/rusty-ferris-club/shellclear"
-    version "0.4.4"    
-    url "https://github.com/rusty-ferris-club/shellclear/releases/download/v0.4.4/shellclear-v0.4.4-x86_64-macos.tar.xz"
-    sha256 "6a70aba5c6cf053570c44f3a26fa82dc2218155c01e4c554383bc3fd53408db8"
+    version "0.4.5"    
+    url "https://github.com/rusty-ferris-club/shellclear/releases/download/v0.4.5/shellclear-v0.4.5-x86_64-macos.tar.xz"
+    sha256 "3f7971a18143951690560b1ef41671f7317d1b3e91ff84b7b23b95180393edf2"
     
     def caveats
       <<~EOS
