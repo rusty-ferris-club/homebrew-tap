658a10ed64f34200e59a3ee19d712ffd54074172
Dotan J. Nahum <jondotan@gmail.com>
2022-10-17 17:42:17 +0300

    Create recon.rb

---
+++ recon.rb
@@ -0,0 +1,11 @@
+class Recon < Formula
+    desc "Find, locate, and query files for ops and security experts"
+    homepage "https://github.com/rusty-ferris-club/recon"
+    version "xxx"    
+    url "xxx"
+    sha256 "xxx"
+
+    def install
+      bin.install "recon"
+    end
+end
