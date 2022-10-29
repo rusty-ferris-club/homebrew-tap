9c8fc66385f8e2c7419c1efb190a00e1d5ddab9a
Dotan J. Nahum <jondotan@gmail.com>
2022-10-29 10:47:18 +0300

    updating formula for recon

--- recon.rb
+++ recon.rb
@@ -1,9 +1,9 @@
 class Recon < Formula
     desc "Find, locate, and query files for ops and security experts"
     homepage "https://github.com/rusty-ferris-club/recon"
-    version "0.1.2"    
-    url "https://github.com/jondot/recon/releases/download/v0.1.2/recon-x86_64-macos.tar.xz"
-    sha256 "09f2b9d67059f72308dfec932d783ee1578c2cef8366c3740a6865944b7b5faf"
+    version "0.5.0"    
+    url "https://github.com/rusty-ferris-club/recon/releases/download/v0.5.0/recon-x86_64-macos.tar.xz"
+    sha256 "ba8a81388c0bb57e320309b9ad77cbf95f8695dcf11bfbc1af6bea06c908e630"
 
     def install
       bin.install "recon"
