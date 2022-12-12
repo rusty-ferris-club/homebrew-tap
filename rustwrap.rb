d2daaa30c8e38c219bfc228a34e902d40ed8d060
Dotan J. Nahum <jondotan@gmail.com>
2022-12-12 19:39:00 +0200

    Create rustwrap.rb

---
+++ rustwrap.rb
@@ -0,0 +1,11 @@
+class Rustwrap < Formula
+    desc "A tool that helps wrap binary releases for easy distribution"
+    homepage "https://github.com/rusty-ferris-club/rustwrap"
+    version "xxxxx"    
+    url "xxxxx"
+    sha256 "xxxxx"
+
+    def install
+      bin.install "rustwrap"
+    end
+end
