1748be659d164bcf5efada05b635e7cc8b65db97
Dotan J. Nahum <jondotan@gmail.com>
2022-12-15 19:28:52 +0200

    updating formula for rustwrap

--- rustwrap.rb
+++ rustwrap.rb
@@ -1,9 +1,9 @@
 class Rustwrap < Formula
     desc "A tool that helps wrap binary releases for easy distribution"
     homepage "https://github.com/rusty-ferris-club/rustwrap"
-    version "1.0.3"    
-    url "https://github.com/rusty-ferris-club/rustwrap/releases/download/v1.0.3/rustwrap-x86_64-macos.tar.xz"
-    sha256 "581112389727faaa9c737a991c3a865d6a84209c1caed8d7518dd6a5beb2e87a"
+    version "1.0.4"    
+    url "https://github.com/rusty-ferris-club/rustwrap/releases/download/v1.0.4/rustwrap-x86_64-macos.tar.xz"
+    sha256 "94b210525ce73e41f5a77e801d3106f611e54ae33d84c8c68b342d9aa6e1c7b8"
 
     def install
       bin.install "rustwrap"
