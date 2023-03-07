5cc24be6a358d907e675e5cd4014c627ca6326cd
Dotan J. Nahum <jondotan@gmail.com>
2023-03-07 14:57:32 +0200

    rustwrap update: rustwrap.rb

--- rustwrap.rb
+++ rustwrap.rb
@@ -1,11 +1,11 @@
 class Rustwrap < Formula
-    desc "A tool that helps wrap binary releases for easy distribution"
-    homepage "https://github.com/rusty-ferris-club/rustwrap"
-    version "1.0.4"    
-    url "https://github.com/rusty-ferris-club/rustwrap/releases/download/v1.0.4/rustwrap-x86_64-macos.tar.xz"
-    sha256 "94b210525ce73e41f5a77e801d3106f611e54ae33d84c8c68b342d9aa6e1c7b8"
+  desc "A tool that helps wrap binary releases for easy distribution"
+  homepage "http://github.com/rusty-ferris-club/rustwrap"
+  url "https://github.com/rusty-ferris-club/rustwrap/releases/download/v1.0.5/rustwrap-x86_64-macos.tar.xz"
+  version "1.0.5"
+  sha256 "d00462fd9b413aedbb733d8c377dbc023aa927646a4813fca6358c9dffb29302"
 
-    def install
-      bin.install "rustwrap"
-    end
+  def install
+    bin.install "rustwrap"
+  end
 end
