c2a9aa54c6267d38e5a83ddd100fed0930dc462c
Dotan J. Nahum <jondotan@gmail.com>
2023-03-07 15:08:50 +0200

    rustwrap update: backpack.rb

--- backpack.rb
+++ backpack.rb
@@ -1,12 +1,11 @@
-class Backpack < Formula
-    desc "Create projects from existing repos"
-    homepage "https://github.com/rusty-ferris-club/backpack"
-    version "2.0.0"
-    url "https://github.com/rusty-ferris-club/backpack/releases/download/v2.0.0/backpack-x86_64-macos.tar.xz"
-    sha256 "bfde4e26070b619dcdd0549dbad06f80b2291c9cb40e63e808620a2bf6391508"
+class backpack < Formula
+  desc "🎒 CLI to create starters from repos + Templates 🤖 + Actions 🚀"
+  homepage "http://github.com/rusty-ferris-club/backpack"
+  url "https://github.com/rusty-ferris-club/backpack/releases/download/v2.1.1/backpack-x86_64-macos.tar.xz"
+  version "2.1.1"
+  sha256 "f48120a385d8631b8e2b4e0df7d7ebe93d6dc38a8daa589bd3713aa3fa9d593b"
 
-    def install
-      bin.install "bp"
-      ln_s bin/"bp", bin/"backpack"
-    end
+  def install
+    bin.install "bp"
+  end
 end
